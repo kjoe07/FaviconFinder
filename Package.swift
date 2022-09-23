257e1c423f343a24a9d3e9ad06a41e84db08a5b5
Yoel Jimenez del Valle <yoel07@gmail.com>
2022-09-23 15:19:45 -0400

    change to iOS 13 minimun

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FaviconFinder",
-    platforms: [.iOS(.v15), .macOS(.v12), .watchOS(.v8), .tvOS(.v15)],
+    platforms: [.iOS(.v13), .macOS(.v12), .watchOS(.v8), .tvOS(.v15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
